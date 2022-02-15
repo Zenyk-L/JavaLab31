e531fc29dcd08e3a95c4dc3733fd6cfbdfd8419d
Zenyk Livchanyn <livchzenyk@gmail.com>
2022-02-16 00:15:42 +0200

    add swagger to user controller

--- Spring-Project/src/main/java/com/epam/project/spring/controller/UserController.java
+++ Spring-Project/src/main/java/com/epam/project/spring/controller/UserController.java
@@ -4,8 +4,8 @@ import com.epam.project.spring.dto.UserDto;
 import com.epam.project.spring.dto.UserWithPasswordDto;
 import com.epam.project.spring.dto.goup.OnCreate;
 import com.epam.project.spring.dto.goup.OnUpdate;
-import com.epam.project.spring.model.User;
 import com.epam.project.spring.service.UserService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -14,36 +14,56 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@Api(tags = "User management API")
+@ApiResponses({
+        @ApiResponse(code = 404, message = "Not found"),
+        @ApiResponse(code = 500, message = "Internal Server Error")
+})
 public class UserController {
 
     private final UserService userService;
 
+
+    @ApiOperation("Get all users")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<UserDto> getAllUser() {
         return userService.listUsers();
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "email", paramType = "path", required = true, value = "User email")
+    })
+    @ApiOperation("Get user by email")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{email}")
     public UserDto getUser(@PathVariable String email) {
         return userService.getUser(email);
     }
 
+    @ApiOperation("Create user")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public UserDto createUser(@RequestBody @Validated(OnCreate.class) UserWithPasswordDto userWithPasswordDto) {
         return userService.createUser(userWithPasswordDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "email", paramType = "path", required = true, value = "User email")
+    })
+    @ApiOperation("Update user by email")
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{email}")
     public UserDto updateUser(@PathVariable String email, @RequestBody @Validated(OnUpdate.class) UserWithPasswordDto userWithPasswordDto) {
         return userService.updateUser(email, userWithPasswordDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "email", paramType = "path", required = true, value = "User email")
+    })
+    @ApiOperation("Delete user by email")
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{email}")
     public void delete(@PathVariable String email) {
